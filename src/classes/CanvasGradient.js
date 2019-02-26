a15fddfc24b2de634fe35a764678808406bf9401
jtenner <tenner.joshua@gmail.com>
2019-02-26 11:13:33 -0500

    Clean up error descriptions, fix some DOMException Types

--- src/classes/CanvasGradient.js
+++ src/classes/CanvasGradient.js
@@ -5,12 +5,12 @@ export default class CanvasGradient {
     this.addColorStop = jest.fn(this.addColorStop.bind(this));
   }
   addColorStop(offset, color) {
-    var numoffset = Number(offset);
-    var colorstr = String(color);
+    const numoffset = Number(offset);
+    const colorstr = String(color);
     if (!Number.isFinite(numoffset) || numoffset < 0 || numoffset > 1) {
       throw new DOMException('IndexSizeError', 'Failed to execute \'addColorStop\' on \'CanvasGradient\': The provided value (\'' + numoffset + '\') is outside the range (0.0, 1.0)');
     }
-    let output = parseColor(colorstr);
+    const output = parseColor(colorstr);
     if (!output.hex) {
       throw new SyntaxError('Failed to execute \'addColorStop\' on \'CanvasGradient\': The value provided (\'' + color + '\') could not be parsed as a color.')
     }
