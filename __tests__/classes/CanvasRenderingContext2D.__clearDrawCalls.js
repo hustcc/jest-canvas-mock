b90f2b3cea811abe69d8225819bd73930e56c9d7
Andy Pearson <andy@andy-pearson.com>
2019-10-25 14:17:47 +0100

    feat(mock): support clearing draw calls and events
    * Adds __clearDrawCalls for clearing draw calls
    * Adds __clearEvents for clearing events
    * Updates README to mention the new functions
    * Adds TypeScript definitions for __clearDrawCalls and __clearEvents

---
+++ __tests__/classes/CanvasRenderingContext2D.__clearDrawCalls.js
@@ -0,0 +1,24 @@
+let ctx;
+beforeEach(() => {
+  // get a new context each test
+  ctx = document.createElement('canvas')
+    .getContext('2d');
+});
+
+afterEach(() => {
+  const drawCalls = ctx.__getDrawCalls();
+  expect(drawCalls).toMatchSnapshot();
+});
+
+describe('__clearDrawCalls', () => {
+  it("should clear the list of draw calls", () => {
+    ctx.fillRect(1, 2, 3, 4);
+    ctx.__clearDrawCalls();
+  })
+
+  it("should not prevent additional draw calls from being collected", () => {
+    ctx.fillRect(1, 2, 3, 4);
+    ctx.__clearDrawCalls();
+    ctx.fillRect(1, 2, 3, 4);
+  })
+})
