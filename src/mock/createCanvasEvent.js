2167daf50dbde9b73370ed24bca9264c49ff4a85
Milan Hauth <milahu@gmail.com>
2020-12-20 17:06:25 +0100

    canvasevent: dont pass identity transform

--- src/mock/createCanvasEvent.js
+++ src/mock/createCanvasEvent.js
@@ -6,7 +6,9 @@
  * @example
  * interface CanvasRenderingContext2DEvent {
  *   type: string;
- *   transform: [number, number, number, number, number, number]; // the resulting current transform
+ *   transform?: [number, number, number, number, number, number];
+ *     // the resulting current transform
+ *     // if undefined, defaults to identity transform [1, 0, 0, 1, 0, 0]
  *   props: {
  *     // if the event is a property was set, `event.props.value` would be set
  *     [propName: string]: any;
@@ -14,5 +16,9 @@
  * }
  */
 export default function createCanvasEvent(type, transform, props) {
+  // do not pass identity transform matrix to make snapshots smaller
+  const [a, b, c, d, e, f] = transform;
+  if (a == 1 && b == 0 && c == 0 && d == 1 && e == 0 && f == 0)
+    return { type, props };
   return { type, transform, props };
 }
