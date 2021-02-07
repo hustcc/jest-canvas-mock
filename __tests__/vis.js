548278e37944ec9f8d378fb36afafcc252e00861
hustcc <i@hust.cc>
2021-02-07 10:34:56 +0800

    test: add test for vis @antv/g2plot (#79)
    * test: add test case for vis library
    
    * fix: typo

---
+++ __tests__/vis.js
@@ -0,0 +1,29 @@
+/**
+ * test vis @AntV/G2Plot
+ */
+
+import { Line } from '@antv/g2plot';
+
+describe('vis', () => {
+  it('vis can pass', () => {
+    const div = document.createElement('canvas');
+
+    const line = new Line(div, {
+      data: [
+        { x: 'A', y: 10 },
+        { x: 'B', y: 20 },
+        { x: 'C', y: 30 },
+      ],
+      xField: 'x',
+      yField: 'y',
+    });
+
+    line.render();
+
+    expect(line.container.querySelector('canvas')).not.toBe(null);
+
+    line.destroy();
+
+    expect(line.container.querySelector('canvas')).toBe(null);
+  });
+});
