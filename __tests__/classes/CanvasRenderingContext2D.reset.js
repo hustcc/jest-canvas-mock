a35ee4f97caed1d2a98cba7c60e4915836bd1693
Chandler Scott <cscott@aquaveo.com>
2024-08-10 13:45:36 -0600

    Add reset

---
+++ __tests__/classes/CanvasRenderingContext2D.reset.js
@@ -0,0 +1,24 @@
+let canvas;
+let ctx;
+
+beforeEach(() => {
+  canvas = document.createElement('canvas');
+  ctx = canvas.getContext('2d');
+  canvas.width = 400;
+  canvas.height = 300;
+});
+
+describe('reset', () => {
+  it('should be a function', () => {
+    expect(typeof ctx.reset).toBe('function');
+  });
+
+  it('should be callable', () => {
+    ctx.rect();
+    expect(ctx.reset).toHaveBeenCalled();
+  });
+
+  it('should throw if any parameters are given', () => {
+    expect(() => ctx.rect(1)).toThrow(TypeError);
+  });
+});
